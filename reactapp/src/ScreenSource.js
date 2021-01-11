283dd8e4455133a68b3240ea302fd7d785d62a83
Tristan Kaffel <tristan.kaffel@gmail.com>
2021-01-11 09:47:05 +0100

    push

--- reactapp/src/ScreenSource.js
+++ reactapp/src/ScreenSource.js
@@ -32,7 +32,7 @@ function ScreenSource(props) {
         var country = 'us'
       }
       props.changeLang(selectedLang)
-      const data = await fetch(`https://newsapi.org/v2/sources?language=${langue}&country=${country}&apiKey=189771adbd2f40d4a27117edd90ff089`)
+      const data = await fetch(`https://newsapi.org/v2/sources?language=${langue}&country=${country}&apiKey=XXXX`)
       const body = await data.json()
       setSourceList(body.sources)
     }
